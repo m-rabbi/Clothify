844188812f879c8ddd5890c0bcd47651166c8ffa
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-24 14:01:43 -0400

    chore(init): set up SwiftUI project with .gitignore and basic structure

---
+++ Clothify/App/ClothifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothifyApp.swift
+//  Clothify
+//
+//  Created by Md Rabbi on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ClothifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
